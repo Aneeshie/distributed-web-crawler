00f3ca7d5f19d7d3db1869af98ac17642210fb54
Aneeshie <aneeshdas556@gmail.com>
2026-04-29 12:41:12 +0000

    feat: add PostgreSQL connection setup

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -1,1 +1,19 @@
 package storage
+
+import "database/sql"
+
+//THE ONLY RESPONSIBILITY IS CONNECT TO POSTGRESQL
+
+func NewPostgresDB(connStr string) *sql.DB {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("failed to open DB:", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("failed to connect DB:", err)
+	}
+
+	return db
+}
