61c95b521de3ba8d040757a55ef07cf3b0d3f1be
Aneeshie <aneeshdas556@gmail.com>
2026-04-29 12:09:47 +0000

    feat: add worker loop for consuming queued URLs

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,54 @@
-package worker
+package main 
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"test/internal/config"
+	"test/internal/queue"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main(){
+	cfg := config.Load()
+
+	fmt.Println("Loaded config")
+
+	//connect to redis
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: cfg.RedisAddr,
+	})
+
+	err := rdb.Ping(context.Background()).Err()
+	if err != nil {
+		log.Fatal("Redis connection failed: ", err)
+	}
+
+	fmt.Println("Connected to Redis")
+
+	q := queue.NewQueue(rdb)
+	for {
+		url, err := q.PopHighest()
+
+		if err != nil {
+			fmt.Println("Queue empty, waiting ....")
+			time.Sleep(3 * time.Second)
+		}
+
+		fmt.Println("Processing: ", url)
+
+		//TODO
+		// fetch page
+		// parse html
+		// save data
+		//enqueue discovered links
+
+		time.Sleep(1 * time.Second)
+
+	}
+
+}
+
